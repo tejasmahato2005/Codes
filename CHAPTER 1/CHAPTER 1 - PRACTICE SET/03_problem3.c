54346177a4f887a1e78da687c6bf93aa70c06f8a
tejasmahato2005 <rohanmahato1234567890@gmail.com>
2025-08-05 18:14:51 +0000

    Add new files

---
+++ CHAPTER 1/CHAPTER 1 - PRACTICE SET/03_problem3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+    int c = 37, f;
+
+        f = ((9.0/5)*c) +32;
+
+        printf("The value in Fahrenheit is %f", f);
+
+
+}
---
+++ CHAPTER 1/CHAPTER 1 - PRACTICE SET/04_problem4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (){
+    int p = 34;
+    int r = 8;
+    int t = 5;
+
+    printf("The value of simple interest is %f", (p*r*t)/100);
+    
+
+}
